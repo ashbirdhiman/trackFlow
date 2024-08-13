962662e378819dec284a207901cce34afd8af925
ashbirdhiman <ashbirashu@gmail.com>
2024-08-13 13:37:23 -0400

    Add initial files

---
+++ src/test/java/com/TrackFlow/ordersystem/TrackFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrackFlow.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
